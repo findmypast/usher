707ebadc1b949b6a0477ca1e3fdc88325d7677e8
Neil Crawford <neil.crawford@gmail.com>
2016-03-25 16:19:21 +0000

    Committing for weekend

---
+++ src/docker-commands/push.js
@@ -0,0 +1,5 @@
+
+
+
+// docker tag -f %docker.repository%-test:%teamcity.build.branch% docker-registry.dun.fh/%docker.repository%-test:%teamcity.build.branch%
+// docker push docker-registry.dun.fh/%docker.repository%-test:%teamcity.build.branch%
