1bcfb759af994a0cc185546ab8f4d2bbdb894af6
Neil Crawford <neil.crawford@gmail.com>
2016-03-28 12:16:06 +0100

    Tidying up and hooking up init and run

--- src/cli.js
+++ src/cli.js
@@ -1,18 +1,20 @@
 #!/usr/bin/env node
 
 const program = require('commander');
+const version = require('./../package.json').version
+const commands = require('./commands')
 
 program
-  .version('0.0.1')
-  .option('init', "Create a `.usher.yml file with some defaults`")
-  .action(function () {
-    let init = require("./commands/init");
-    init();
-  })
-  .option('run [commands...]', "Run commands contained in `.usher.yml`")
-  .action(function (commands) {
-    let run = require("./commands/run");
-    run(commands);
-  });
+  .version(require('./../package.json').version)
+
+program
+  .command('init')
+  .description('Initialise your project for use with Usher')
+  .action(commands.init);
+
+program
+  .command('run [commands...]')
+  .description('Run one or multiple Usher commands')
+  .action(commands.run);
 
 program.parse(process.argv);
