6c5d576a4bc9ae05b1faadecbdbcbf6c1e7e264c
Mike Thomas <mthomas@findmypast.com>
2016-04-13 12:41:19 +0100

    Add support for docker run command.
    Pair: Jae, Sash

---
+++ src/factories/docker/run.js
@@ -0,0 +1,29 @@
+// https://docs.docker.com/engine/reference/commandline/run/
+
+"use strict";
+
+const _ = require('lodash');
+const buildCommand = require('../buildCommand');
+const buildImageName = require('../buildImageName');
+
+const executable = "docker run";
+
+const optionMap = {
+  environment:  values  => _.map(
+                          values, item => `-e ${item}`)
+                          .join(" "),
+  name:       value   => `--name ${value}`,
+  memory:     value   => `-m ${value}`,
+  publish:    values  => _.map(values, item => `-p ${item.host}:${item.client}`)
+                        .join(" ")
+};
+
+function getTarget(config) {
+  return buildImageName(config);
+}
+
+module.exports = (config) => {
+  const options = _.omit(config, 'target', 'container_command');
+  const target = getTarget(config.target);
+  return buildCommand(executable, options, optionMap, target, config.container_command);
+};
