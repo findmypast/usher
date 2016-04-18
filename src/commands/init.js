8afe0281c37d2e7cce5f9a86968acf1be6447fc5
sashman <sashman90@gmail.com>
2016-04-18 16:29:36 +0100

    Adding guards for non existent templates

--- src/commands/init.js
+++ src/commands/init.js
@@ -6,15 +6,27 @@ let inquirer = require('inquirer');
 const path = require('path');
 const handlebars = require('handlebars');
 
+function guardFileExists(fileName, message){
+  try {
+      fs.statSync(fileName);
+  } catch (e) {
+      throw message;
+  };
+}
+
 module.exports = (templateName, opts) => {
   const templatePath = path.resolve(opts.path || `${__dirname}/templates`);
   const usherFilePath = path.resolve(opts.outputPath || `${__dirname}`);
   const templateFilename = `${templatePath}/template.${templateName}.yml`;
   const jsonPromptFilename = `${templatePath}/prompt.${templateName}.json`;
+
+  guardFileExists(jsonPromptFilename, `Template prompts file does not exist, cannot find ${jsonPromptFilename}`);
+  guardFileExists(templateFilename, `Template file does not exist, cannot find  ${templateFilename}`);
   const prompts = fs.readJsonSync(jsonPromptFilename);
 
   return inquirer.prompt(prompts)
     .then(answers => {
+
       const source = fs.readFileSync(templateFilename, 'utf-8');
       const template = handlebars.compile(source);
       const result = template(answers);
