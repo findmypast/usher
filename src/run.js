64d5f597872b432bfd2814ce0e752951002fa844
Jae Bach Hardie <jhardie@findmypast.com>
2016-04-21 17:20:33 +0100

    Now takes lists of environment variables to pass on to Spawn
    Paired: danger_mike

--- src/run.js
+++ src/run.js
@@ -12,14 +12,26 @@ function expandTokens(command, vars) {
   return template(vars);
 }
 
+function resolveKeyValuePairs(array, vars) {
+  const templated = vars ? _.map(array, a => expandTokens(a, vars)) : array;
+  const split = _.map(templated, a => a.split('='));
+  const hashed = _.fromPairs(split);
+  return hashed;
+}
+
+function buildSpawnOptions(env) {
+  return !_.isEmpty(env) ? { env: env } : {};
+}
+
 module.exports = (taskName, taskVars, opts) => {
   const usherFile = opts.filepath || '.usher.yml';
-  const splitVars = _.map(taskVars, a => a.split('='));
-  const parsedVars = _.fromPairs(splitVars);
+  const parsedVars = resolveKeyValuePairs(taskVars);
   const config = parser.safeLoad(fs.readFileSync(usherFile, 'utf8'));
   const vars = _.merge(config.vars, parsedVars);
-  const command = expandTokens(config.tasks[taskName].cmd, vars);
-  const commandArray = command.split(" ");
+  const task = config.tasks[taskName];
+  const command = expandTokens(task.cmd, vars).split(" ");
+  const parsedEnv = resolveKeyValuePairs(task.environment, vars);
+  const spawnOptions = buildSpawnOptions(parsedEnv)
 
-  spawnSync(commandArray[0], _.tail(commandArray));
+  spawnSync(command[0], _.tail(command), spawnOptions)
 }
--- test/commands_from_yaml.spec.js
+++ test/commands_from_yaml.spec.js
@@ -14,14 +14,41 @@ chai.use(sinonChai);
 describe('Given a YAML file run command execution', () => {
   let filename = 'test/test.usher.yml';
 
-  let tests = [{
+  let tests = [
+    {
       key: 'build',
       cmdArgs: ['version=latest'],
       expected: {
           executable: 'docker',
-          args: 'build --force-rm -t docker-registry.dun.fh/findmypast/usher:latest .'.split(' ')
+          args: 'build --force-rm -t docker-registry.dun.fh/findmypast/usher:latest .'.split(' '),
+          options: {}
       }
-  }];
+    },
+    {
+      key: 'build',
+      cmdArgs: ['version=latest', 'image=usher-test'],
+      expected: {
+          executable: 'docker',
+          args: 'build --force-rm -t docker-registry.dun.fh/usher-test:latest .'.split(' '),
+          options: {}
+      }
+    },
+    {
+      key: 'publish',
+      cmdArgs: ['version=latest', 'user=neil', 'password=password', 'email=ncrawford@findmypast.com'],
+      expected: {
+          executable: 'docker',
+          args: 'run --name usher-publisher --rm docker-registry.dun.fh/findmypast/usher:latest npm run publish-to-npm'.split(' '),
+          options: {
+            env: {
+              NPM_USER: 'neil',
+              NPM_PASSWORD: 'password',
+              NPM_EMAIL: 'ncrawford@findmypast.com'
+            }
+          }
+      }
+    }
+  ];
 
   let spawnSyncStub = sinon.stub();
   run.__set__({
@@ -37,8 +64,8 @@ describe('Given a YAML file run command execution', () => {
   })
 
   tests.forEach( test =>
-    it(`should make command for preset ${test.key}`, () => {
+    it(`should execute task ${test.key} with command line vars ${test.cmdArgs.join(' ')}`, () => {
       run(test.key, test.cmdArgs, {filepath:filename})
-      expect(spawnSyncStub).to.have.been.calledWith(test.expected.executable, test.expected.args);
+      expect(spawnSyncStub).to.have.been.calledWith(test.expected.executable, test.expected.args, test.expected.options);
     }));
 });
