27e828be5b7f29fe2f924d28c2596e4e2eb3af14
sashman <sashman90@gmail.com>
2016-06-28 15:55:03 +0100

    Adding style with chalk

--- src/list.js
+++ src/list.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const parser = require('js-yaml');
+const chalk = require('chalk');
 const fs = require('fs');
 const _ = require('lodash');
 const getTaskConfig = require('./modules/get-task-config');
@@ -10,11 +11,11 @@ function listAll(opts) {
   const usherFile = opts.filepath || '.usher.yml';
   const usherConfigs = parser.safeLoad(fs.readFileSync(usherFile, 'utf8'));
 
-  logger.info(`All tasks for ${usherFile}:`);
+  logger.info(chalk.bold(`All tasks for ${usherFile}:`));
   _.forOwn(usherConfigs.tasks, (config, key) => {
     const descriptionTask = _.find(config, (t) => t.description);
     const description = _.get(descriptionTask, 'description', '');
-    logger.info(`${key} - ${description}`);
+    logger.info(`${chalk.underline(key)} - ${description}`);
   });
 }
 
