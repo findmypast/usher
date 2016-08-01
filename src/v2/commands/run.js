fe5278a275e85899a041cc78ae7e7cfa95b49783
Jae Bach Hardie <jhardie@findmypast.com>
2016-08-01 15:58:17 +0100

    Run command orchestrator implemented

---
+++ src/v2/commands/run.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const Promise = require('bluebird');
+const _ = require('lodash');
+const parse = require('./parse');
+const setup = require('./setup');
+const task = require('../core/task');
+const loggers = require('../loggers');
+
+const DEFAULT_FILE = 'usher.yml';
+const DEFAULT_LOGGER = 'console';
+
+function parseVars(varList) {
+  return {vars: _.fromPairs(_.map(varList, varPair => _.split(varPair, '=')))};
+}
+
+module.exports = (taskName, taskVars, opts) => Promise.try(() => {
+  const file = opts.file || DEFAULT_FILE;
+  const loggerName = opts.logger || DEFAULT_LOGGER;
+  const logger = _.get(loggers, loggerName);
+  const config = _.merge({}, parse(file, logger), parseVars(taskVars));
+  return setup(config, logger)
+  .then(state => task(state.get(taskName), state));
+});
---
+++ src/v2/commands/run.spec.js
@@ -0,0 +1,74 @@
+/* global describe before after beforeEach it expect sandbox mockery mocks _*/
+'use strict';
+
+describe('factories/task', function() {
+  let sut;
+  const setup = sandbox.stub();
+  const parse = sandbox.stub();
+  const task = sandbox.stub();
+  const logger = mocks.logger;
+  const otherLogger = sandbox.stub();
+  beforeEach(function() {
+    sandbox.reset();
+  });
+  before(function() {
+    mockery.enable();
+    mockery.warnOnUnregistered(false);
+    mockery.registerMock('./setup', setup);
+    mockery.registerMock('./parse', parse);
+    mockery.registerMock('../core/task', task);
+    mockery.registerMock('../loggers', {console: logger, other: otherLogger});
+
+    sut = require('./run');
+  });
+  after(function() {
+    mockery.deregisterAll();
+    mockery.disable();
+  });
+  describe('given valid input', function() {
+    const taskName = 'test_task';
+    const taskVars = ['var=value', 'var2=value2'];
+    const opts = {
+      file: 'foo.yml',
+      logger: 'other'
+    };
+    const config = {
+      vars: {
+        var: 'wrong',
+        var3: 'value3'
+      },
+      tasks: {
+        test_task: {
+          do: 'test'
+        }
+      }
+    };
+    parse.returns(config);
+    setup.returns(Promise.resolve({get: name => _.get(config.tasks, name)}));
+    it('calls parse on the file name defined in opts', function() {
+      return sut(taskName, taskVars, opts).then(() => expect(parse).to.be.calledWith(opts.file, otherLogger));
+    });
+    it('calls parse on "usher.yml" if no file option', function() {
+      return sut(taskName, taskVars, {}).then(() => expect(parse).to.be.calledWith('usher.yml', logger));
+    });
+    it('calls parse with console logger if no logger option', function() {
+      return sut(taskName, taskVars, {}).then(() => expect(parse).to.be.calledWith('usher.yml', logger));
+    });
+    it('parses CLI vars and merges them into config before calling setup', function() {
+      return sut(taskName, taskVars, {}).then(() => expect(setup).to.be.calledWith({
+        tasks: config.tasks,
+        vars: {
+          var: 'value',
+          var2: 'value2',
+          var3: 'value3'
+        }
+      }, logger));
+    });
+    it('given no vars calls setup with config', function() {
+      return sut(taskName, [], {}).then(() => expect(setup).to.be.calledWith(config, logger));
+    });
+    it('runs the named task', function() {
+      return sut(taskName, taskVars, {}).then(() => expect(task).to.be.calledWith(_.get(config.tasks, taskName)));
+    });
+  });
+});
