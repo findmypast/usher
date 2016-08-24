d842a76597cadcaf9c8e5d7244f75733e5a9415d
Kim McCann <kimberleylmccann@gmail.com>
2016-08-24 15:26:54 +0100

    Added dereferencing for parallel and sequence tasks

--- src/v2/tasks/sequence.js
+++ src/v2/tasks/sequence.js
@@ -5,5 +5,5 @@ const task = require('../core/task');
 
 module.exports = (state) => Promise.try(() => {
   const taskActions = state.peek().actions;
-  return Promise.mapSeries(taskActions, step => task(step, state));
+  return Promise.mapSeries(state.dereference(taskActions), step => task(step, state));
 });
