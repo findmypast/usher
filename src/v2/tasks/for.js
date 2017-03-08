e82e19f761f2412a314292a5f71e25608e2adaa4
rixka <rixka@outlook.com>
2017-03-08 15:28:41 +0000

    add option to run tasks sequentially in for loop
    Paired: Josh

--- src/v2/tasks/for.js
+++ src/v2/tasks/for.js
@@ -7,17 +7,33 @@ const task = require('../core/task');
 const ACCEPTED_OPTIONS = [
   'every',
   'in',
-  'exec'
+  'exec',
+  'options'
 ];
 
 function optionAsArray(option) {
   return _.isArray(option) ? option : option.split(',');
 }
 
+function runParallel(settings, state) {
+  return Promise.map(optionAsArray(settings.in), value => task({
+    description: `${settings.exec} with ${settings.every}=${value}`,
+    do: settings.exec,
+    [settings.every]: value}, _.cloneDeep(state)));
+}
+
+function runSequential(settings, state) {
+  return Promise.each(optionAsArray(settings.in), value => task({
+    description: `${settings.exec} with ${settings.every}=${value}`,
+    do: settings.exec,
+    [settings.every]: value}, _.cloneDeep(state)));
+}
+
 module.exports = (state) => Promise.try(() => {
-  const options = _.reduce(ACCEPTED_OPTIONS, (result, value) => _.set(result, value, state.get(value)), {});
-  return Promise.map(optionAsArray(options.in), value => task({
-    description: `${options.exec} with ${options.every}=${value}`,
-    do: options.exec,
-    [options.every]: value}, _.cloneDeep(state)));
+  const settings = _.reduce(ACCEPTED_OPTIONS, (result, value) => _.set(result, value, state.get(value)), {});
+  const options = settings.options || {};
+
+  const run = options.sequential ? runSequential : runParallel;
+
+  return run(settings, state);
 });
