48581e813d8a5ab37f0d21a2d7270956bdd5d2b8
Donald Robertson <donaldarobertson89@gmail.com>
2018-10-29 15:37:49 +0000

    SPA1580: List CLI handler now using top level lib and directing to v3 command handler

--- src/list.js
+++ src/list.js
@@ -1,34 +1,41 @@
 'use strict';
 
-const firstline = require('firstline');
+const chalk = require('chalk');
+const getVersion = require('./lib/get-version');
+const listView = require('./list-view');
+const Logger = require('./v2/loggers/default');
 const v1 = require('./v1/list');
 const v2 = require('./v2/commands/list');
-const listView = require('./list-view');
-const chalk = require('chalk');
-const Logger = require('./v2/loggers/default');
+
+const listCommandsByVersion = {
+  1: require('./v1/list'),
+  2: require('./v2/commands/list'),
+  3: require('./v3/commands/list')
+}
+
 const logger = new Logger();
 
-function isV2(file) {
-  return firstline(file).then(line => line.match(/version.*'2'/));
+async function getListCommandForVersion(filename) {
+  const version = await getVersion(filename);
+
+  return version === '0' ? null : listCommandsByVersion[_version];
 }
 
-function checkVersion(fileName) {
-  return isV2(fileName)
-  .then(result => {
-    if (result) {
-      return v2;
-    }
-    return v1;
-  });
-}
+module.exports = async function(taskName, opts) {
+  const listCommand = await getListCommandForVersion(opts.file);
 
-module.exports = (taskName, opts) => {
-  return checkVersion(opts.file)
-    .catch(() => checkVersion('.usher.yml'))
-    .catch(() => checkVersion('usher.yml'))
-    .then(list => list(taskName, opts))
-    .then(tasksAndTheirDescriptions => {
-      logger.info(chalk.bold(`Listing tasks under ${taskName ? taskName : (opts.file || '.usher.yml')}:`));
-      listView(logger.info, tasksAndTheirDescriptions);
-    });
+  if (listCommand == null) {
+    const msg = opts.file
+      ? `The command 'usher list' could not be run for the specified file, please ensure a valid version is specified`
+      : `The command 'usher list' could not be run, please ensure an usher file with a valid version exists in the current directory`;
+
+    logger.info(chalk.red(chalk.bold(msg)));
+
+    return;
+  }
+
+  const list = await listCommand(taskName, opts);
+  logger.info(chalk.bold(`Listing tasks under ${taskName ? taskName : (opts.file || '.usher.yml')}:`));
+  
+  listView(logger.info, list);
 };
