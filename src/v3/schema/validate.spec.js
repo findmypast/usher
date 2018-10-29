1cdd9bc93cc5c71924ffb30789e6c8cb8f6095ee
Donald Robertson <donaldarobertson89@gmail.com>
2018-10-29 15:36:08 +0000

    SPA1580: Renamed spec file to match implementation

--- src/v3/schema/validate.spec.js
+++ src/v3/schema/validate.spec.js
@@ -1,10 +1,11 @@
 /* eslint-disable strict */
 
+const includeSchema = require('./spec-data/include-schema');
 const minimalSchema = require('./spec-data/minimal-schema');
 const noIncludeSchema = require('./spec-data/no-include-schema');
 const validate = require('./validate');
 
-describe('validate', function() {
+describe('v3/schema/validate', function() {
   test('a minimal schema is valid', function() {
     const valid = validate(minimalSchema);
     expect(valid).toBeTruthy();
@@ -14,4 +15,9 @@ describe('validate', function() {
     const valid = validate(noIncludeSchema);
     expect(valid).toBeTruthy();
   });
+
+  test('a schema with include directives is valid', function() {
+    const valid = validate(noIncludeSchema);
+    expect(valid).toBeTruthy();
+  })
 });
