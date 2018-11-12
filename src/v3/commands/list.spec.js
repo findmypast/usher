20e14cb3b8917ba15daa287874dfc12729bc6c67
Donald Robertson <donaldarobertson89@gmail.com>
2018-11-12 10:54:29 +0000

    Tweaking v3 list command test to exercise all branches

--- src/v3/commands/list.spec.js
+++ src/v3/commands/list.spec.js
@@ -5,8 +5,11 @@ const path = require('path');
 
 describe('v3/commands/list', () => {
   describe('given valid input', () => {
-    const expected = require('./spec-data/list-response.json');
     const file = path.join(__dirname, 'spec-data', 'list.yml');
+    const expected = {
+      obtain_grafana_api_key: 'Obtains the Grafana API key from Vault',
+      publish_dashboard_to_grafana: ''
+    };
 
     test('lists all tasks in usher file', async function() {
       var opts = { file };
