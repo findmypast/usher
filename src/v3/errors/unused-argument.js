6717c5c55ea21b2e5562228a230985ffd31fc5dd
Donald Robertson <donaldarobertson89@gmail.com>
2018-12-10 13:29:30 +0000

    SPA1580: Adding error for unused arguments

---
+++ src/v3/errors/unused-argument.js
@@ -0,0 +1,9 @@
+/* eslint-disable strict */
+
+class UnusedArgumentError extends Error {
+  constructor(unusedArgument, task) {
+    super(`the argument ${unusedArgument} supplied to task ${task} is not used.`);
+  }
+}
+
+module.exports = UnusedArgumentError;
