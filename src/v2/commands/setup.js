4d82db5838cdefa82949aa88f397ccf601c959d7
RonanQuigley <info@ronanquigley.com>
2023-09-07 16:38:16 +0100

    SPAN-000: Temp fix to sort out timeouts talking to github
    Not sure what's going on but we're getting loads of timeouts with npm installs via git
    
    Co-authored-by: Attila Molnar
    Co-authored-by: Chris Destouni

--- src/v2/commands/setup.js
+++ src/v2/commands/setup.js
@@ -1,169 +1,186 @@
-'use strict';
+"use strict";
 
-const _ = require('lodash');
-const Promise = require('bluebird');
-const exec = require('child-process-promise').exec;
-const State = require('../core/state');
-const installDir = require('../core/installed-modules').installDir;
-const defaultTasks = { tasks: require('../tasks') };
-const InvalidConfigError = require('../lib/errors').InvalidConfigError;
-const path = require('path');
+const _ = require("lodash");
+const Promise = require("bluebird");
+const exec = require("child-process-promise").exec;
+const State = require("../core/state");
+const installDir = require("../core/installed-modules").installDir;
+const defaultTasks = { tasks: require("../tasks") };
+const InvalidConfigError = require("../lib/errors").InvalidConfigError;
+const path = require("path");
 
 const varRegexp = /<%=(.*?)%>/g;
 
 function tasksHaveDo(config) {
-  _.mapValues(config.tasks, (task, key) => {
-    if (_.has(task, 'tasks')) {
-      tasksHaveDo(task.tasks);
-    }
-    else {
-      if (!_.has(task, 'do')) {
-        throw new InvalidConfigError(`task ${key} missing "do" property`);
-      }
-    }
-  });
+	_.mapValues(config.tasks, (task, key) => {
+		if (_.has(task, "tasks")) {
+			tasksHaveDo(task.tasks);
+		} else {
+			if (!_.has(task, "do")) {
+				throw new InvalidConfigError(`task ${key} missing "do" property`);
+			}
+		}
+	});
 }
 
 const validators = {
-  varsIsObject: config => {
-    if (!_.isPlainObject(config.vars)) {
-      throw new InvalidConfigError('vars not a valid object');
-    }
-  },
-  tasksIsObject: config => {
-    if (!_.isPlainObject(config.tasks)) {
-      throw new InvalidConfigError('tasks not a valid object');
-    }
-  },
-  includeIsArray: config => {
-    if (config.include && !_.isArray(config.include)) {
-      throw new InvalidConfigError('include must be an array');
-    }
-  },
-  includesHaveProperties: config => {
-    if (!_.every(config.include, object => _.has(object, 'from'))) {
-      throw new InvalidConfigError('includes must all have "from"');
-    }
-  },
-  tasksHaveDo: config => {
-    tasksHaveDo(config);
-  }
+	varsIsObject: (config) => {
+		if (!_.isPlainObject(config.vars)) {
+			throw new InvalidConfigError("vars not a valid object");
+		}
+	},
+	tasksIsObject: (config) => {
+		if (!_.isPlainObject(config.tasks)) {
+			throw new InvalidConfigError("tasks not a valid object");
+		}
+	},
+	includeIsArray: (config) => {
+		if (config.include && !_.isArray(config.include)) {
+			throw new InvalidConfigError("include must be an array");
+		}
+	},
+	includesHaveProperties: (config) => {
+		if (!_.every(config.include, (object) => _.has(object, "from"))) {
+			throw new InvalidConfigError('includes must all have "from"');
+		}
+	},
+	tasksHaveDo: (config) => {
+		tasksHaveDo(config);
+	}
 };
 
 function installModule(moduleName) {
-  if (_.endsWith(moduleName, '.yml')) {
-    return Promise.resolve();
-  }
-  return exec(`npm install ${moduleName} --prefix ${installDir()}`);
+	if (_.endsWith(moduleName, ".yml")) {
+		return Promise.resolve();
+	} else if (moduleName.includes("usher_shared_tasks.git")) {
+		return Promise.resolve();
+	}
+	return exec(`npm install ${moduleName} --prefix ${installDir()}`);
 }
 
 function requireModule(requireName) {
-  return require(`${installDir()}/node_modules/${requireName}`);
+	return require(`${installDir()}/node_modules/${requireName}`);
 }
 
 function loadAndParseYmlFile(filename, propertyName, parser) {
-  const file = parser(filename);
-  return file[propertyName];
+	const file = parser(filename);
+	return file[propertyName];
 }
 
 function importTasks(tasks) {
-  const builtTasks = {};
+	const builtTasks = {};
 
-  _.each(_.keys(tasks), taskName => {
-    _.set(builtTasks, taskName, _.get(tasks, taskName));
-  });
+	_.each(_.keys(tasks), (taskName) => {
+		_.set(builtTasks, taskName, _.get(tasks, taskName));
+	});
 
-  return builtTasks;
+	return builtTasks;
 }
 
 function getAlias(name) {
-  const split = _.split(name, ' as ');
+	const split = _.split(name, " as ");
 
-  if (split.length === 1) {
-    split.push(name); // Alias name same as name if no alias specified
-  }
+	if (split.length === 1) {
+		split.push(name); // Alias name same as name if no alias specified
+	}
 
-  return split;
+	return split;
 }
 
 function importTasklist(taskList, taskConfig, usherFilePath, parser) {
-  const [importName, aliasName] = getAlias(taskConfig.name || taskConfig.from);
-  const tasks = _.endsWith(taskConfig.from, '.yml')
-    ? loadAndParseYmlFile(path.join(usherFilePath, taskConfig.from), 'tasks', parser)
-    : requireModule(importName);
+	const [importName, aliasName] = getAlias(taskConfig.name || taskConfig.from);
+	const tasks = _.endsWith(taskConfig.from, ".yml")
+		? loadAndParseYmlFile(
+				path.join(usherFilePath, taskConfig.from),
+				"tasks",
+				parser
+		  )
+		: requireModule(importName);
 
-  if (!taskConfig.import) {
-    taskList[aliasName] = {
-      tasks: importTasks(tasks)
-    };
-  }
-  else {
-    taskList[aliasName] = {
-      tasks: {}
-    };
-    _.each(taskConfig.import, taskImportName => {
-      const propertyAliasName = getAlias(taskImportName)[1];
-      const propertyTaskName = getAlias(taskImportName)[0];
+	if (!taskConfig.import) {
+		taskList[aliasName] = {
+			tasks: importTasks(tasks)
+		};
+	} else {
+		taskList[aliasName] = {
+			tasks: {}
+		};
+		_.each(taskConfig.import, (taskImportName) => {
+			const propertyAliasName = getAlias(taskImportName)[1];
+			const propertyTaskName = getAlias(taskImportName)[0];
 
-      taskList[aliasName].tasks[propertyAliasName] = {
-        tasks: importTasks(tasks[propertyTaskName].tasks)
-      };
-    });
-  }
+			taskList[aliasName].tasks[propertyAliasName] = {
+				tasks: importTasks(tasks[propertyTaskName].tasks)
+			};
+		});
+	}
 
-  return taskList;
+	return taskList;
 }
 
 function importVariables(varList, config, usherFilePath, parser) {
-  const [importName] = getAlias(config.name || config.from);
-  const isYmlFile = _.endsWith(config.from, '.yml');
+	const [importName] = getAlias(config.name || config.from);
+	const isYmlFile = _.endsWith(config.from, ".yml");
 
-  let variables = {};
-  if (isYmlFile) {
-    const filename = path.join(usherFilePath, config.from);
-    variables = loadAndParseYmlFile(filename, 'vars', parser);
-  }
-  else {
-    let mod = requireModule(importName);
-    _.each(config.import, taskImportName => {
-      const propertyTaskName = getAlias(taskImportName)[0];
-      variables = _.merge(variables, mod[propertyTaskName].vars || {});
-    });
-  }
+	let variables = {};
+	if (isYmlFile) {
+		const filename = path.join(usherFilePath, config.from);
+		variables = loadAndParseYmlFile(filename, "vars", parser);
+	} else {
+		let mod = requireModule(importName);
+		_.each(config.import, (taskImportName) => {
+			const propertyTaskName = getAlias(taskImportName)[0];
+			variables = _.merge(variables, mod[propertyTaskName].vars || {});
+		});
+	}
 
-
-
-  varList = _.merge(varList, variables || {});
-  return varList;
+	varList = _.merge(varList, variables || {});
+	return varList;
 }
 
 function interpolateModulesToInstall(config) {
-  return _.map(config.include, (include) => {
-    let module = _.get(include, 'from');
-    let match = varRegexp.exec(module);
+	return _.map(config.include, (include) => {
+		let module = _.get(include, "from");
+		let match = varRegexp.exec(module);
 
-    while (match != null) {
+		while (match != null) {
+			if (config.vars && config.vars[match[1]]) {
+				module = module.replace(match[0], config.vars[match[1]]);
+			}
 
-      if (config.vars && config.vars[match[1]]) {
-        module = module.replace(match[0], config.vars[match[1]]);
-      }
-      
-      match = varRegexp.exec(module);
-    }
+			match = varRegexp.exec(module);
+		}
 
-    return module;
-  });
+		return module;
+	});
 }
 
-module.exports = (config, Logger, usherFilePath, parser = require('./parse')) => Promise.try(() => {
-  _.mapValues(validators, validator => validator(config));
-  const modulesToInstall = interpolateModulesToInstall(config);
-  return Promise.all(_.map(modulesToInstall, installModule))
-    .then(() => {
-      const reducedTasks = _.reduce(config.include, (acc, includeConfig) => importTasklist(acc, includeConfig, usherFilePath, parser), {});
-      const reducedArgs = _.reduce(config.include, (acc, includeConfig) => importVariables(acc, includeConfig, usherFilePath, parser), {});
-      const initialState = _.merge({}, defaultTasks, reducedArgs, config.vars, _.pick(config, 'tasks'), { tasks: reducedTasks });
+module.exports = (config, Logger, usherFilePath, parser = require("./parse")) =>
+	Promise.try(() => {
+		_.mapValues(validators, (validator) => validator(config));
+		const modulesToInstall = interpolateModulesToInstall(config);
+		return Promise.all(_.map(modulesToInstall, installModule)).then(() => {
+			const reducedTasks = _.reduce(
+				config.include,
+				(acc, includeConfig) =>
+					importTasklist(acc, includeConfig, usherFilePath, parser),
+				{}
+			);
+			const reducedArgs = _.reduce(
+				config.include,
+				(acc, includeConfig) =>
+					importVariables(acc, includeConfig, usherFilePath, parser),
+				{}
+			);
+			const initialState = _.merge(
+				{},
+				defaultTasks,
+				reducedArgs,
+				config.vars,
+				_.pick(config, "tasks"),
+				{ tasks: reducedTasks }
+			);
 
-      return new State(initialState, Logger);
-    });
-});
+			return new State(initialState, Logger);
+		});
+	});
